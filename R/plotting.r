a0da4ba2e6a8541837e4d7bc5b1b750147eccd74
Ursula Laa <uschi.laa@gmx.at>
2020-11-17 14:16:37 +1100

    Removing old plotting code.

--- R/plotting.r
+++ R/plotting.r
@@ -9,24 +9,4 @@ scagnostics_pairs <- function(scag, columns = 1:9, interactive = FALSE){
     # using GGally
     GGally::ggpairs(aes(text = label), columns = columns) %>%
     plotly::ggplotly()
-    # fully plotly version from Di's tutorial
-    #plotly::highlight_key(~label) %>%
-    #plotly::plot_ly() %>% 
-    #plotly::add_trace(
-    #  type = 'splom',
-    #  dimensions = list(
-    #    list(label='Outlying', values=~Outlying),
-    #    list(label='Skewed', values=~Skewed),
-    #    list(label='Clumpy', values=~Clumpy),
-    #    list(label='Sparse', values=~Sparse),
-     #   list(label='Striated', values=~Striated),
-    #    list(label='Convex', values=~Convex),
-    #    list(label='Skinny', values=~Skinny),
-    #    list(label='Stringy', values=~Stringy),
-    #    list(label='Monotonic', values=~Monotonic)
-    #  ),
-    #  text=~label
-    #) %>%
-    # highlighting not working for SLOM?
-    #plotly::highlight(on = "plotly_click", off = "plotly_deselect", color="red")
 }
